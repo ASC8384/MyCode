5c8a6e797ab5971d1119d4b23c1d161760e01407
ASC_8384 <ASC_8384@foxmail.com>
2017-10-10 16:23:08 +0800

    Create Dijkstra_simple.cpp

---
+++ Graph/Dijkstra_simple.cpp
@@ -0,0 +1,29 @@
+void dijkstra(void){
+	memset(map, 1, sizeof map);
+	memset(djk, 1, sizeof djk);
+	for(int i = 1; i <= n; i++)
+		djk[i] = map[s][i];
+	djk[s] = 0;
+	use[s] = 1;
+	int pos = 0;
+	int tmp = 0x7f;
+	int cnt = 1;
+	while(cnt < n){
+		for(int i = 1; i <= n; i++)
+			if(!use[i] && djk[i] < tmp){
+				pos = i;
+				tmp = djk[i];
+			}
+		if(!pos)
+			NULL;
+		use[pos] = 1;
+		for(int i = 1; i <= n; i++)
+			if(djk[i] > djk[pos] + map[pos][i])
+				djk[i] = djk[pos] + map[pos][i];
+		tmp = 0x7f;
+		pos = 0;
+		cnt++;
+	}
+}//djk[t] 答案
+//s 进，t 出
+//map 邻接矩阵
