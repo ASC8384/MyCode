5ec44595437209ed683438746979f0406346ed38
ASC_8384 <ASC_8384@foxmail.com>
2017-10-10 18:15:51 +0800

    Update Dijkstra_simple.cpp

--- Graph/Dijkstra_simple.cpp
+++ Graph/Dijkstra_simple.cpp
@@ -1,29 +1,29 @@
-void dijkstra(void){
-	//预先 memset(map, 1, sizeof map);
-	memset(djk, 1, sizeof djk);
-	for(int i = 1; i <= n; i++)
-		djk[i] = map[s][i];
-	djk[s] = 0;
-	use[s] = 1;
-	int pos = 0;
-	int tmp = 0x7f;
-	int cnt = 1;
-	while(cnt < n){
-		for(int i = 1; i <= n; i++)
-			if(!use[i] && djk[i] < tmp){
-				pos = i;
-				tmp = djk[i];
-			}
-		if(!pos)
-			NULL;
-		use[pos] = 1;
-		for(int i = 1; i <= n; i++)
-			if(djk[i] > djk[pos] + map[pos][i])
-				djk[i] = djk[pos] + map[pos][i];
-		tmp = 0x7f;
-		pos = 0;
-		cnt++;
-	}
+void dijkstra(int s, int t){
+//预先 memset(map, 0x7f, sizeof map);
+    memset(djk, 0x7ffffff, sizeof djk);
+    for(int i = 1; i <= n; i++)
+        djk[i] = map[s][i];
+    djk[s] = 0;
+    use[s] = 1;
+    int pos = 0;
+    int tmp = 0x7ffffff;
+    int cnt = 1;
+    while(cnt < n){
+        for(int i = 1; i <= n; i++)
+            if(!use[i] && djk[i] < tmp){
+                pos = i;
+                tmp = djk[i];
+            }
+        if(!pos)
+            NULL;
+        use[pos] = 1;
+        for(int i = 1; i <= n; i++)
+            if(djk[i] > djk[pos] + map[pos][i])
+                djk[i] = djk[pos] + map[pos][i];
+        tmp = 0x7ffffff;
+        pos = 0;
+        cnt++;
+    }
 }//djk[t] 答案
 //s 进，t 出
 //map 邻接矩阵
