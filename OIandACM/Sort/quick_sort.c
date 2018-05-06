7091a7b931a8cc269f6b20227b1ce9cb5ac9d46c
ASC_8384 <ASC_8384@foxmail.com>
2018-05-06 16:13:53 +0800

    'QuickSort'

---
+++ OIandACM/Sort/quick_sort.c
@@ -0,0 +1,26 @@
+//int a[sizesize];
+void quicksort(int left,int right)
+{
+    int tl = left;
+    int tr = right;
+    int mid = a[(left+right)/2];
+    int t;
+    do
+    {
+        while(a[tl] < mid) tl++;
+        while(a[tr] > mid) tr--;
+        if (tl <= tr)
+        {
+            t = a[tl];
+            a[tl] = a[tr];
+            a[tr] = t;
+            tr--;
+            tl++;
+        }
+    } while (tl < tr);
+    if (tl < right) quicksort(tl,right);
+    if (tr > left) quicksort(left,tr);
+}
+//quicksort(1,n);
+
+//从1开始,到n结束
