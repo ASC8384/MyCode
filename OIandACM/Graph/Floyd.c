43d1c35240c8a26a0cb072d094d5ee34095d5e98
ASC_8384 <ASC_8384@foxmail.com>
2018-06-29 22:53:41 +0800

    Feat: 添加正常版本

--- OIandACM/Graph/Floyd.c
+++ OIandACM/Graph/Floyd.c
@@ -1,3 +1,16 @@
+/**********************************************************************
+*                              Floyd.c                               *
+**********************************************************************/
+/* 
+** Copyright	Human
+** Author		ASC_8384
+** Mail			ASC_8384@foxmail.com
+** Website		http://ASC8384.top
+** FileName		Floyd.c
+** Description	Floyd-Warshall算法 求最短路径
+** HomepageURL	https://github.com/ASC8384/MyCodeSnippets
+** License		CC0
+*/
 for(k = 1; k <= n; k++)
 	for(j = 1; j <= n; j++)
 		if(k != j)
@@ -9,3 +22,21 @@ for(k = 1; k <= n; k++)
 
 //存图于dis[][]
 //计算的是图中任意一点到其余任意一点的距离
+
+for (int k = 1; k <= n; k++)
+	for (int i = 1; i <= n; i++)
+		if (k != i)
+			for (int j = 1; j <= n; j++)
+				if (j != k)
+					if (i != j)
+						if (dis[i][j] > dis[i][k] + dis[k][j])
+							dis[i][j] = dis[i][k] + dis[k][j];
+
+/* 
+let dist be a |V| × |V| array of minimum distances initialized to ∞ (infinity)
+
+for each vertex v
+	dist[v][v] ← 0
+for each edge (u,v)
+	dist[u][v] ← w(u,v)
+*/
