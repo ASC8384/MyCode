de538bb7ec65f10489e749cea2e260122344f1b6
ASC_8384 <ASC_8384@foxmail.com>
2018-07-19 17:27:59 +0800

    Modified: filename

---
+++ SmallTool/Python/zhihu/basic_information.py
@@ -0,0 +1,24 @@
+from zhihu_oauth import ZhihuClient
+
+client = ZhihuClient()
+
+client.load_token('token.pkl')
+
+me = client.me()
+
+print('name', me.name)
+print('headline', me.headline)
+print('description', me.description)
+
+print('following topic count', me.following_topic_count)
+print('following people count', me.following_count)
+print('followers count', me.follower_count)
+
+print('voteup count', me.voteup_count)
+print('get thanks count', me.thanked_count)
+
+print('answered question', me.answer_count)
+print('question asked', me.question_count)
+print('collection count', me.collection_count)
+print('article count', me.articles_count)
+print('following column count', me.following_column_count)
