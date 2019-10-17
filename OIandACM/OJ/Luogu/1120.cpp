99b4fb48431335a8b9f4107dbbd28f69602a900a
ASC_8384 <ASC_8384@foxmail.com>
2019-10-17 12:52:38 +0800

    Create: the Sticks on 2019-10-11

---
+++ OIandACM/OJ/Luogu/1120.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, len;
+int a[99];
+bool f[99];
+int sum;
+int last;
+int top;
+
+bool cmp(int x, int y){
+	return x > y;
+}
+
+inline int init(void){
+	int x;
+	char c;
+	for(c = getchar(); !isdigit(c); c = getchar());
+	for(x = 0; isdigit(c); c = getchar())
+		x = x * 10 + c - '0';
+	return x;
+}
+
+bool dfs(int resum, int relen){
+	if(resum == 0 && relen == 0){
+		return true;
+	}else{
+		if(relen == 0)
+			relen = len;
+		for(int i = (relen != len ? last + 1 : 1); i <= top; i++){
+			if(!f[i] && a[i] <= relen){
+				if(a[i] == a[i - 1] && !f[i - 1])
+					continue;
+				last = i;
+				f[i] = true;
+				if(dfs(resum - 1, relen - a[i]))
+					return true;
+				f[i] = false;
+				if(relen == a[i] || relen == len)
+					return false;//
+			}
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	//while(cin >> n){
+	//while(n = init()){
+	n = init();{
+		bool over = true;
+		sum = 0;
+		top = 0;
+		for(int i = 1; i <= n; i++){
+			int tmp = init();
+			//cin >> tmp;
+			if(tmp <= 50){
+				a[++top] = tmp;
+				sum += a[top];
+			}
+		}
+		sort(a + 1, a + n + 1, cmp);
+		int tmp = sum >> 1;
+		for(len = a[1]; len <= tmp; len++){//
+			if(sum % len)//
+				continue;
+			//memset(f, false, sizeof(f));
+			last = 1;
+			if(dfs(top, len)){
+				cout << len << endl;
+				over = false;
+				//printf("%d\n", len);
+				break;
+			}
+		}
+		if(over)
+			cout << sum << endl;
+	}
+	return 0;
+}
---
+++ OIandACM/OJ/Luogu/1120_45.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, len;
+int a[99];
+bool f[99];
+int sum;
+
+bool cmp(int x, int y){
+	return x > y;
+}
+
+bool dfs(int resum, int relen){
+	if(resum == 0 && relen == 0){
+		return true;
+	}else{
+		if(relen == 0)
+			relen = len;
+		for(int i = 1; i <= n; i++){
+			if(!f[i] && a[i] <= relen){
+				if(a[i] == a[i - 1] && !f[i - 1])
+					continue;
+				f[i] = true;
+				if(dfs(resum - 1, relen - a[i]))
+					return true;
+				f[i] = false;
+				if(relen == a[i] || relen == len)
+					return false;//
+			}
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	while(cin >> n){
+		sum = 0;
+		int cnt = 0;
+		for(int i = 1; i <= n; i++){
+			int tmp;
+			cin >> tmp;
+			if(tmp <= 50)
+				a[++cnt] = tmp;
+			sum += a[cnt];
+		}
+		sort(a + 1, a + n + 1, cmp);
+		for(len = a[1]; len <= (sum >> 1); len++){//
+			if(sum % len)//
+				continue;
+			memset(f, false, sizeof(f));
+			if(dfs(n, len)){
+				cout << len << endl;
+				//printf("%d\n", len);
+				break;
+			}
+		}
+	}
+	return 0;
+}
