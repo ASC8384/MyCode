2bd142ace7881d7f7ce6da6200304cd2679bf8cc
ASC_8384 <ASC_8384@foxmail.com>
2020-03-04 22:45:34 +0800

    Create: 单元最短路（弱化版）

---
+++ OIandACM/OJ/Luogu/3371.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int u[500008], v[500008], w[500008];
+long long dis[10008];
+
+const long long inf=2147483647; // 0x7fffffff; //(1 << 31) - 1; // 0x7fffffff;
+
+int main() {
+	int n, m, s;
+	scanf("%d%d%d", &n, &m, &s);
+
+	for(int i = 1; i <= m; i++)
+		scanf("%d%d%d", u + i, v + i, w + i);
+
+	for(int i = 1; i <= n; i++)
+		dis[i] = inf;
+	dis[s] = 0;
+
+	for(int k = 1, is = 1; k < n && is; k++) {
+		is = 0;
+		for(int i = 1; i <= m; i++) {
+			if(dis[v[i]] > dis[u[i]] + w[i]) {
+				dis[v[i]] = dis[u[i]] + w[i];
+				is		  = 1;
+			}
+		}
+	}
+	for(int i = 1; i <= n; i++)
+		printf("%lld ", dis[i]);
+
+	return 0;
+}
