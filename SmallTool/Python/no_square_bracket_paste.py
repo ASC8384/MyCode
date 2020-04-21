354fd54224b08e7136c55b6f7a1450d847893017
ASC_8384 <ASC_8384@foxmail.com>
2020-04-21 09:59:27 +0800

    Fix: 误删('【','】')

--- SmallTool/Python/no_square_bracket_paste.py
+++ SmallTool/Python/no_square_bracket_paste.py
@@ -3,7 +3,7 @@
 @License      : CC0
 @Website      : http://www.ASC8384.top
 @Date         : 2020-04-01 22:17:30
-@LastEditTime : 2020-04-02 21:43:27
+@LastEditTime : 2020-04-06 20:40:16
 @Description  : win 下去除剪切板中的[]、【】符号，转为两个空格
 '''
 
@@ -17,7 +17,7 @@ import re
 # @return: string
 ###
 def no_square_brackets(source):
-    source = re.sub('(【.*】)', "  ", source)
+    #  source = re.sub('(【.*】)', "  ", source)
     source = re.sub('(\[.*\])', '  ', source)
     return source
 
@@ -31,6 +31,7 @@ if __name__ == "__main__":
                 recent_value = no_square_brackets(recent_value)
                 pyperclip.copy(recent_value)
                 print("Value changed: %s" % str(recent_value))
+                print("Now paste value: %s" % str(pyperclip.paste()))
                 flag_value = recent_value
         except KeyboardInterrupt:
             break
