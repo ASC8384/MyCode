f655d9a578a1cd3cabfa04ebfac4bbda4de6fc84
ASC_8384 <ASC_8384@foxmail.com>
2020-05-03 13:58:07 +0800

    Fix: 重复启动（根据名字）

--- SmallTool/Shell/autostart.sh
+++ SmallTool/Shell/autostart.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+ret=$(ps ax | grep -v grep | grep wp-autochange)
+if [ "$ret" != "" ]; then
+	exit
+fi
+
 /bin/bash ~/code/dwm/sh/wp-autochange.sh &
 /bin/bash ~/code/dwm/sh/dwm-status.sh &
 /bin/bash ~/code/dwm/sh/two-finger-scroll.sh &
 
 picom -b --config ~/.config/picom.conf
+
