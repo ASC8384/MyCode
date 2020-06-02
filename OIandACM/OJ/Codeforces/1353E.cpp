72a76106d9b8df099b282d02d1dae91b1b2f6acd
ASC_8384 <ASC_8384@foxmail.com>
2020-06-02 23:55:13 +0800

    Add: over Codeforces Round #642 (Div. 3)

--- OIandACM/OJ/Codeforces/1353E.cpp
+++ OIandACM/OJ/Codeforces/1353E.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int size = 1e6+9;
+int qm[size];
+int f[size];
+
 int main() {
 	cin.tie(0);
 	ios::sync_with_stdio(0);
@@ -8,15 +12,27 @@ int main() {
 	cin >> t;
 	while(t--) {
 		int k, n;
-		cin >> k >> n;
+		cin >> n >> k;
+		// cin >> k >> n;
 		string s;
 		cin >> s;
-		int ans = 0;
-        int ll = 0;
-		int rr = 0;
-		for(int i = 0; i < n; i++){  
-            if(s[i] == '1')
+		int ans = n;
+		// k++;
+		// k--;
+		qm[0] = s[0] - '0';
+		for(int i = 1; i < n; i++){  
+            qm[i] = qm[i-1] + s[i] - '0';
         }
+		for(int i = 0; i < n; i++){
+			f[i] = qm[i] - (s[i] == '1');
+			if(i - k >= 0){
+				f[i] = min(f[i],
+						f[i - k] + (s[i] != '1') + qm[i - 1] - qm[i - k]);
+			}
+			ans = min(ans, f[i] + qm[n-1] - qm[i]);
+		}
+		// for(int i = 0; i < n; i++)printf("%d:f_%d q_%d\n", i, f[i], qm[i]);
+		cout << ans << endl;
 	}
 	return 0;
 }
--- OIandACM/OJ/Codeforces/1353F.cpp
+++ OIandACM/OJ/Codeforces/1353F.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 long long a[109][109];
 long long f[109][109];
+int n, m;
+
+long long solve(long long now){
+	// memset(f, 0x3f, sizeof(f));
+	for(int i = 1; i <= n; i++)
+		for(int j = 1; j <= m; j++)
+			f[i][j] = 0x7ffffffffffffff;
+	f[1][1] = 0;
+	for(int i = 1; i <= n; i++)
+		for(int j = 1; j <= m; j++)
+			if(now + i + j <= a[i][j]){
+				f[i][j] += a[i][j] - now - i - j;
+				if(i + 1 <= n)
+					f[i + 1][j] = min(f[i][j], f[i + 1][j]);
+				if(j + 1 <= m)
+					f[i][j + 1] = min(f[i][j], f[i][j + 1]);
+			}else {
+				f[i][j] = 0x7ffffffffffffff;
+			}
+	return f[n][m];
+}
 
 int main() {
 	cin.tie(0);
@@ -10,18 +31,18 @@ int main() {
 	int t;
 	cin >> t;
 	while(t--) {
-		int n, m;
 		cin >> n >> m;
 		for(int i = 1; i <= n; i++)
 			for(int j = 1; j <= m; j++)
 				cin >> a[i][j];
-		long long ans = 0;
-		memset(f, 0, sizeof(f));
-		for(int i = n; i > 0; i--) {
-			for(int j = n; j > 0; j--) {
-                f[i][j] = 
+		long long ans = 0x7ffffffffffffff;
+		// memset(f, 0, sizeof(f));
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= m; j++) {
+				ans = min(ans, solve(a[i][j] - i - j));
 			}
 		}
+		cout << ans << endl;
 	}
 	return 0;
 }
