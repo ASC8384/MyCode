71c7e5dfecfd5da740d5651c6c887ecb66e75d39
ASC_8384 <ASC_8384@foxmail.com>
2020-06-15 19:48:04 +0800

    Add: 自动创建比赛文件

---
+++ SmallTool/Python/new_contest.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+src = '''#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(0);
+	int t;
+	cin >> t;
+	while(t--) {
+		;
+	}
+}
+'''
+
+
+def create_file(src, name, num=6):
+    for i in range(num):
+        now = name + chr(i + ord('A')) + '.cpp'
+        try:
+            with open(now, 'x') as f:
+                print('now create:' + now)
+                f.write(src)
+        except FileExistsError:
+            print('error: ' + now + ' Exists')
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) == 3):
+        create_file(src, sys.argv[1], int(sys.argv[2]))
+    elif (len(sys.argv) == 2):
+        create_file(src, sys.argv[1])
+    else:
+        s = input().split()
+        if (len(s) == 1):
+            create_file(src, s[0])
+        else:
+            create_file(src, s[0], int(s[1]))
