950f72b7120fb42fe59d89790498c163842c7288
ASC_8384 <ASC_8384@foxmail.com>
2020-06-24 00:24:16 +0800

    Add: improve the template

--- SmallTool/Python/new_contest.py
+++ SmallTool/Python/new_contest.py
@@ -3,16 +3,21 @@ import sys
 
 src = '''#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+const int size = 2e5+9;
+const ll mod = 19260817;
 
 int main() {
 	cin.tie(0);
 	cout.tie(0);
 	ios::sync_with_stdio(0);
+
 	int t;
 	cin >> t;
 	while(t--) {
 		;
 	}
+    return 0;
 }
 '''
 
