0719aeba0faa135a591fd1d8a63f61e0fd209747
ASC_8384 <ASC_8384@foxmail.com>
2020-06-24 10:41:09 +0800

    Add:  Codeforces Round #652 (Div. 2)  4/6, rank 658, +97

---
+++ OIandACM/OJ/Codeforces/1369A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int		  size = 2e5 + 9;
+const ll		  mod  = 19260817;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(0);
+
+	int t;
+	cin >> t;
+	while(t--) {
+		ll n;
+		cin >> n;
+		if(n % 4 == 0)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+}
