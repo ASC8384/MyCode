2095b65deb9d1eb04a0baaec5b47a9d0b03a31b8
ASC_8384 <ASC_8384@foxmail.com>
2020-08-23 22:09:20 +0800

    Fix: matrix

--- OIandACM/Math/matrix.cpp
+++ OIandACM/Math/matrix.cpp
@@ -10,10 +10,10 @@ struct mat {
 	int n, m;
 	mat(int x = 0, int y = 0) : n(x), m(y) {}
 	void prt() {
-		printf("%d %d\n", n, m);
+		printf("%lld %lld\n", n, m);
 		for(int i = 1; i <= n; i++) {
 			for(int j = 1; j <= m; j++) {
-				printf("%d ", a[i][j]);
+				printf("%lld ", a[i][j]);
 			}
 			putchar('\n');
 		}
@@ -29,7 +29,8 @@ struct mat {
 					ret.a[i][j] = (ret.a[i][j] + a[i][k] * x.a[k][j] % mod) % mod;
 		return ret;
 	}
-	friend mat operator^(mat &x, ll t) { // 矩阵快速幂
+	friend mat operator^(mat &y, ll t) { // 矩阵快速幂
+		mat x	= y;
 		mat ret = mat(x.n, x.n);
 		memset(ret.a, 0, sizeof(ret.a));
 		for(int i = 1; i <= x.n; i++) {
