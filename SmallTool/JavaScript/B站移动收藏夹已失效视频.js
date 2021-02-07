0f45faf1c958b28cc8d96d7728e3f7de751d0f91
ASC_8384 <29878345+ASC8384@users.noreply.github.com>
2021-02-07 18:50:12 +0800

    Add: 添加自动翻页功能

--- SmallTool/JavaScript/B站移动收藏夹已失效视频.js
+++ SmallTool/JavaScript/B站移动收藏夹已失效视频.js
@@ -4,12 +4,16 @@ function sleep(ms) {
 
 async function move() {
   const t = setInterval(async function() {
-    $("li.small-item.disabled").find(".video-check-container").click();
-    $(".fav-action-fixtop > ul:nth-child(1) > li:nth-child(4)").click();
-    // sleep(500);
-    await sleep(1000);
-    $("li.target-favitem:nth-child(10)").click(); // replace the number you want
-    $("div.modal-container:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > a:nth-child(1) > span:nth-child(1)").click();
+    if ($("li.small-item.disabled").find(".video-check-container").length > 0) {
+      $("li.small-item.disabled").find(".video-check-container").click();
+      $(".fav-action-fixtop > ul:nth-child(1) > li:nth-child(4)").click();
+      // sleep(500);
+      await sleep(1000);
+      $("li.target-favitem:nth-child(10)").click(); // replace the number you want
+      $("div.modal-container:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > a:nth-child(1) > span:nth-child(1)").click();
+    } else {
+      $(".be-pager-next").click();
+    }
     await sleep(1000);
   }, 1300)
 }
